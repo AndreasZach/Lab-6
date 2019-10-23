c3c739c511241a305c7b0f383e192f6b07007488
Anders Orlinder <anders_orlinder@hotmail.com>
2019-10-23 14:24:41 +0200

    Added timeclass and randomgeneratorclass

---
+++ Lab6/RandomIntGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public static class RandomIntGenerator
+    {
+        private static Random randomGenerator = new Random();
+
+        public static int GetRandomInt(int from, int to)
+        {
+            int randomInt = randomGenerator.Next(from, to);
+            return randomInt;
+        }
+    }
+}
