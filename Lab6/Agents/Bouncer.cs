e319ebead95c4340c61f45869bd81f0cbbcc2edb
Andreas <Andreas_Zachrisson@hotmail.com>
2019-10-23 11:31:41 +0200

    Reorganized initial code according to new planning.

--- Lab6/Agents/Bouncer.cs
+++ Lab6/Agents/Bouncer.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab6.Agents
+namespace Lab6
 {
     class Bouncer : Agent
     {
-        public List<string> patronNames;
+        //Add random name generator method to fill the List. Need to make it many names, so as to never let it get empty.
+        public ConcurrentBag<string> patronNames = new ConcurrentBag<string>(); 
+
+        public Patron AllowPatronEntry()
+        {
+            string name;
+            patronNames.TryTake(out name);
+            if (name == null)
+                return null;
+            return new Patron(name);
+        }
     }
 }
