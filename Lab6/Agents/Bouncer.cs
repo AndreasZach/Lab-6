f7e8dfff0a4a030170704353df24b9037f2a9f10
Andreas <Andreas_Zachrisson@hotmail.com>
2019-10-23 14:24:54 +0200

    Started work on Pub.cs. Added working functionality to Bouncer class.

--- Lab6/Agents/Bouncer.cs
+++ Lab6/Agents/Bouncer.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab6
@@ -10,17 +11,22 @@ namespace Lab6
     class Bouncer : Agent
     {
         //Add random name generator method to fill the List. Need to make it many names, so as to never let it get empty.
-        public ConcurrentBag<string> patronNames = new ConcurrentBag<string>(); 
+        public ConcurrentBag<string> patronNames = new ConcurrentBag<string>();
+        public int minInterval { get; set; }
+        public int maxInterval { get; set; }
 
         //TODO: Make it so that thread continues after returning a Patron
 
-        public Patron AllowPatronEntry()
+        public void AllowPatronEntry(ConcurrentQueue<Patron> queueToBar)
         {
-            string name;
-            patronNames.TryTake(out name);
-            if (name == null)
-                return null;
-            return new Patron(name);
+            while (LeftPub != true)
+            {
+                string name;
+                patronNames.TryTake(out name);
+                if (name != null)
+                    queueToBar.Enqueue(new Patron(name));
+                Thread.Sleep((RandomTidMetod(minInterval,maxInterval)) * 1000);
+            }
         }
     }
 }
